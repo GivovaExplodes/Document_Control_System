69a22f952a53321bb02067368d7da02a2e85be91
Michael Byrnes <114934559+GivovaExplodes@users.noreply.github.com>
2023-11-15 00:21:16 +0000

    Updated Views, Controllers & 1st working prototype (#7)
    Co-authored-by: Cian Perill <20278667@studentmail.ul.ie>

---
+++ LibraryManagementSystem/Controllers/LibraryController.cs
@@ -0,0 +1,43 @@
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using LibraryManagementSystem.Models; // Adjust this namespace to match your project structure
+
+public class LibraryController : Controller
+{
+    private readonly LibraryModel _library;
+
+    public LibraryController()
+    {
+        // Initialize your LibraryModel instance
+        _library = new LibraryModel();
+    }
+
+    // Action method for displaying all books
+    [HttpPost]
+    public IActionResult Index()
+    {
+        var allBooks = _library.GetAllBooks();
+        return View(allBooks);
+    }
+
+    // Action method for checking out a book
+    [HttpPost]
+    public IActionResult CheckOutBook(int bookId)
+    {
+        _library.CheckOutBook(bookId);
+        // Implement the logic for checking out a book
+        // For simplicity, let's just remove the book for now
+        
+        // Redirect back to the Index action
+        return RedirectToAction("Index");
+    }
+
+    // Action method for showing checked-out books
+    public IActionResult CheckedOutBooks()
+    {
+        // Implement the logic for showing checked-out books
+        // For simplicity, let's just return a view with all books
+        var checkedOutBooks = _library.GetAllBooks();
+        return View(checkedOutBooks);
+    }
+}
---
+++ LibraryManagementSystem/Controllers/MainMenuController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using LibraryManagementSystem.Models; // Adjust this namespace to match your project structure
+
+public class MainMenuController : Controller
+{
+    // Action method for displaying the login view
+    public IActionResult Login()
+    {
+        var loginModel = new LoginModel();
+        return View(loginModel);
+    }
+
+    // Action method for handling the login form submission
+    [HttpPost]
+    public IActionResult Login(LoginModel loginModel)
+    {
+        // Validate the loginModel, authenticate the user, and redirect accordingly
+        if (ModelState.IsValid)
+        {
+            
+            // For demonstration purposes, redirect to the Index action with a sample user
+            var userModel = new UserModel
+            {
+                Id = 1,
+                Username = "SampleUser",
+                Name = "John Doe",
+                LoyaltyPts = 100,
+                Role = UserModel.UserRole.patron
+            };
+
+            return RedirectToAction("Index", userModel);
+        }
+
+        // If the model is not valid, return the login view with validation errors
+        return View(loginModel);
+    }
+
+    // Action method for displaying the main menu view
+    public IActionResult Index(UserModel userModel)
+    {
+        // Pass the user model to the view
+        return View(userModel);
+    }
+
+    // You can add more action methods as needed for different functionalities
+}
